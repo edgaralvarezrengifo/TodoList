3ecde5138d7d05a27ae4f608d1d2ebd3f2baf245
Edgar <edgare192@hotmail.com>
2015-05-04 00:17:19 -0500

    Revert "Ultimo"
    This reverts commit 75bdae613bb0aba7032ceae18bd3fe3bae10cba4.

--- app/models/todo.rb
+++
@@ -1,5 +0,0 @@
-class Todo < ActiveRecord::Base
-    
-	validates :todo_item, presence: true
-
-end
--- db/migrate/20150504012735_add_dead_line_to_todos.rb
+++
@@ -1,5 +0,0 @@
-class AddDeadLineToTodos < ActiveRecord::Migration
-  def change
-    add_column :todos, :deadLine, :date
-  end
-end
